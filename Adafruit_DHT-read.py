748df3bf3de42eebca68030df4a2d5d437857b5f
Wook Hyun <superwhyun@gmail.com>
2020-08-16 23:51:03 +0900

    add

---
+++ Adafruit_DHT-read.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import Adafruit_DHT
+
+sensor = Adafruit_DHT.DHT11
+
+# GPIO23 (pin no: #16)
+pin = 23
+
+while True:
+  print "probing"
+  humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+  if humidity is not None and temperature is not None:
+    print "Temp={0:0.1f}*C Humidity={1:0.1f}%".format(temperature, humidity)
+  else:
+    print "Failed to get reading."
